7c7628b7646f606490ff970a50e04c4f891ce542
Nilesh Prasad <nilesh@cityflo.com>
2023-08-05 17:20:27 +0530

    server up and running with Logger and Recovery middleware already attached

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"go-crud-gin/helper"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-
+	log.Info().Msg("Started server!")
 	routes := gin.Default()
 	routes.GET("", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, "welcome home")
@@ -18,4 +20,5 @@ func main() {
 	}
 
 	err := server.ListenAndServe()
+	helper.ErrorPanic(err)
 }
