73c35ebcc0a57d0ebeb7a2cff668b889ce8e17b8
Nilesh Prasad <nilesh@cityflo.com>
2023-08-05 19:52:44 +0530

    service created

---
+++ service/tags_service.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+	"go-crud-gin/data/request"
+	"go-crud-gin/data/response"
+)
+
+type TagsService interface {
+	Create(tags request.CreateTagsRequest)
+	Update(tags request.UpdateTagsRequest)
+	Delete(tagsId int)
+	FindById(tagsId int) response.TagsResponse
+	FindAll() []response.TagsResponse
+}
---
+++ service/tags_service_impl.go
@@ -0,0 +1,70 @@
+package service
+
+import (
+	"go-crud-gin/data/request"
+	"go-crud-gin/data/response"
+	"go-crud-gin/helper"
+	"go-crud-gin/model"
+	"go-crud-gin/repository"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type TagsServiceImpl struct {
+	TagsRepository repository.TagsRepository
+	Validate       *validator.Validate
+}
+
+// Create implements TagsService.
+func (t *TagsServiceImpl) Create(tags request.CreateTagsRequest) {
+	err := t.Validate.Struct(tags)
+	helper.ErrorPanic(err)
+	tagModel := model.Tags{
+		Name: tags.Name,
+	}
+	t.TagsRepository.Save(tagModel)
+}
+
+// Delete implements TagsService.
+func (t *TagsServiceImpl) Delete(tagsId int) {
+	t.TagsRepository.Delete(tagsId)
+}
+
+// FindAll implements TagsService.
+func (t *TagsServiceImpl) FindAll() []response.TagsResponse {
+	result := t.TagsRepository.FindAll()
+	var tags []response.TagsResponse
+	for _, value := range result {
+		tag := response.TagsResponse{
+			Id:   value.Id,
+			Name: value.Name,
+		}
+		tags = append(tags, tag)
+	}
+	return tags
+}
+
+// FindById implements TagsService.
+func (t *TagsServiceImpl) FindById(tagsId int) response.TagsResponse {
+	tagData, err := t.TagsRepository.FindById(tagsId)
+	helper.ErrorPanic(err)
+	return response.TagsResponse{
+		Id:   tagData.Id,
+		Name: tagData.Name,
+	}
+}
+
+// Update implements TagsService.
+func (t *TagsServiceImpl) Update(tags request.UpdateTagsRequest) {
+	tagData, err := t.TagsRepository.FindById(tags.Id)
+	helper.ErrorPanic(err)
+	tagData.Name = tags.Name
+	t.TagsRepository.Update(tagData)
+}
+
+func newTagsServiceImpl(tagRepository repository.TagsRepository, validate *validator.Validate) TagsService {
+	return &TagsServiceImpl{
+		TagsRepository: tagRepository,
+		Validate:       validate,
+	}
+}
